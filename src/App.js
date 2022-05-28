6537c6af6cab6e208083810188a979aff1faace9
Nushrat-Nokshi <nushratahmed1814@gmail.com>
2022-05-28 20:57:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,57 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const  Products =[
+  {name: 'Chocolect' , Price:'$20.33'},
+  {name: 'Chips' , Price:'$10.44'} ,
+  {name: 'Ice-Cream' , Price:'$30.00'} ,
+  {name: 'coco-cola' , Price:'$80.03'}]
+  const list =[
+    'Nushrat' , 'Ratul_H', 'Urmila' ,'Najmul','Shanzida' ,'Hridoy','M_Riya' ,'M.Sajal'
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>REACT</h1>
+       <p>I start It</p>
+       <Product name={Products[0].name} Price={Products[0].Price}></Product>
+        <Product name={Products[1].name} Price={Products[1].Price}></Product>
+        <Product name={Products[2].name} Price={Products[2].Price}></Product>
+        <Product name={Products[3].name} Price={Products[3].Price}></Product>
+      
+      <ul>
+        {
+          list.map(list => <li>{list}</li>)
+        }
+        {
+           Products.map( Product => <li>{ Product.name}</li>)
+        }
+      </ul>
+     
+      
       </header>
     </div>
   );
 }
+
+function Product(props){
+  const ProductStyle={
+    border: '1px solid black',
+    borderRadius:'5px',
+    backgroundColor:'lightgray',
+    height:'200px',
+    width:'200px',
+    float:'left',
+    color: 'black',
+  }
+return (
+  <div style={ProductStyle}>
+    <h3>{props.name}</h3>
+    <h5>{props.Price}</h5>
+    <button>Buy Now</button>
+  </div>
+)
+}
+
 
 export default App;
